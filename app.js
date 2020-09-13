60e12e44a5d9b8d168d84fa0ed0dd70b7c3e1254
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-09-13 18:00:20 +0000

    deploy: 043252becb65bc3c940cd32f81ac83c5ed956b5e

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // Your Code Here
+
+function alphabetize(word) {
+  return word.toLowerCase().split("").sort().join("").trim();
+}
+
+function getAnagramsOf(text) {
+  let comparison = alphabetize(text);
+    let anagramsArray = []
+  if (word of words.js == comparison.length) {
+      let compared = alphabetize(word)
+      if (compared == comparison) {
+        anagramsArray.push(words.js[word])
+    }
+  }
+  let displayAnagrams = document.createElement("div")
+  displayAnagrams.innerText = JSON.stringify(anagramsArray)
+  document.body.appendChild(displayAnagrams);
+}
+
+const button = document.getElementById("findButton");
+button.onclick = function () {
+  let typedText = document.getElementById("input").value;
+  getAnagramsOf(typedText);
+};
